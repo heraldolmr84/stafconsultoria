70207b5b901b8aed84564a7faaa0ef15c2d75ca3
heraldolmr84 <heraldoribeiro84@gmail.com>
2023-02-26 22:01:58 -0400

    commit 1

---
+++ js/script.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+
+    //paralax
+    setTimeout(function() {
+
+        $("#valores").parallax({imageSrc: 'img/fundo-valores.png'});
+        $("#img-contact").parallax({imageSrc: 'img/contato2.jpg'});
+    //$('#contato').parallax({imageSrc: 'img/cont-img.png'});
+    }, 250)
+});
